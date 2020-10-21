0245b2030836559558bc0098ca7fc1113089a4ce
Fly Li <fly.li@eoitek.com>
2020-10-21 16:16:58 +0800

    优化DecodeRequestBody和WrapMsg接口

--- model/request.go
+++ model/request.go
@@ -1,17 +1,20 @@
 package model
 
 import (
+	"bufio"
 	jsoniter "github.com/json-iterator/go"
-	"io/ioutil"
 	"net/http"
 )
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func DecodeRequestBody(request *http.Request, v interface{}) error {
-	body, err := ioutil.ReadAll(request.Body)
-	if err != nil {
+	body := make([]byte, request.ContentLength)
+
+	r := bufio.NewReader(request.Body)
+	if _, err := r.Read(body); err != nil {
 		return err
 	}
+
 	return json.Unmarshal(body, v)
 }
--- model/response.go
+++ model/response.go
@@ -6,13 +6,29 @@ import (
 	"net/http"
 )
 
-func WrapMsg(c *gin.Context, code int, msg string, data interface{}) {
-	c.JSON(http.StatusOK, gin.H{
+func WrapMsg(c *gin.Context, code int, msg string, data interface{}) error {
+	c.Status(http.StatusOK)
+	c.Header("Content-Type", "application/json; charset=utf-8")
+
+	jsonBytes, err := json.Marshal(map[string]interface{}{
 		"code": code,
 		"msg":  msg,
 		"data": data,
 	})
+	if err != nil {
+		log.Logger.Errorf("%s %s marshal response body fail: %s", c.Request.Method, c.Request.RequestURI, err.Error())
+		return err
+	}
+
+	_, err = c.Writer.Write(jsonBytes)
+	if err != nil {
+		log.Logger.Errorf("%s %s write response body fail: %s", c.Request.Method, c.Request.RequestURI, err.Error())
+		return err
+	}
+
 	if code != SUCCESS {
 		log.Logger.Errorf("%s %s return %d, %v", c.Request.Method, c.Request.RequestURI, code, data)
 	}
+
+	return nil
 }
