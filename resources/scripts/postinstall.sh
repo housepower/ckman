4fb14b9a9f4f12d83cf4a70e7f8c38cc41021f37
YenchangChan <chenyanchang1990@163.com>
2022-08-02 17:24:41 +0800

    fix: upgrade cause permission denied issue

--- resources/scripts/postinstall.sh
+++ resources/scripts/postinstall.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 mkdir -p /var/log/ckman /run/ckman
-chown -R ckman:ckman /var/log/ckman /etc/ckman
-
-chmod -R 664 /var/log/ckman /etc/ckman
-chmod 750 /var/log/ckman /etc/ckman /etc/ckman/conf /etc/ckman/package /etc/ckman/dbscript
+chown -R ckman:ckman /var/log/ckman /etc/ckman /run/ckman
+chmod 750 /var/log/ckman /etc/ckman
 
 /bin/systemctl daemon-reload
 /bin/systemctl enable ckman
