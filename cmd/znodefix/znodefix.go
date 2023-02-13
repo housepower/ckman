8e764cfeaf5263586dce1559d7becb5f23bc39fe
YenchangChan <chenyanchang1990@163.com>
2023-02-13 18:01:45 +0800

    refactor: extract znodefix function

--- cmd/znodefix/znodefix.go
+++ cmd/znodefix/znodefix.go
@@ -8,20 +8,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path"
 
-	"github.com/go-zookeeper/zk"
 	"github.com/housepower/ckman/common"
 	"github.com/housepower/ckman/config"
 	"github.com/housepower/ckman/log"
-	"github.com/housepower/ckman/model"
 	"github.com/housepower/ckman/repository"
 	_ "github.com/housepower/ckman/repository/dm8"
 	_ "github.com/housepower/ckman/repository/local"
 	_ "github.com/housepower/ckman/repository/mysql"
 	_ "github.com/housepower/ckman/repository/postgres"
 	"github.com/housepower/ckman/service/zookeeper"
-	"github.com/pkg/errors"
 )
 
 type CmdOptions struct {
@@ -86,55 +82,13 @@ func main() {
 	if err != nil {
 		log.Logger.Fatalf("get cluster %s failed:%v", cmdOps.ClusterName, err)
 	}
-	err = CleanZoopath(cluster)
+	service, err := zookeeper.NewZkService(cluster.ZkNodes, cluster.ZkPort)
+	if err != nil {
+		log.Logger.Fatalf("can't create zookeeper instance:%v", err)
+	}
+	err = service.CleanZoopath(cluster, cmdOps.ClusterName, cmdOps.Node, cmdOps.Dryrun)
 	if err != nil {
 		log.Logger.Fatalf("clean zoopath error:%v", err)
 	}
 	log.Logger.Info("znode fixed successfully")
-}
-
-func CleanZoopath(conf model.CKManClickHouseConfig) error {
-	svr, err := zookeeper.NewZkService(conf.ZkNodes, conf.ZkPort)
-	if err != nil {
-		return err
-	}
-
-	root := fmt.Sprintf("/clickhouse/tables/%s", cmdOps.ClusterName)
-	return clean(svr, root)
-}
-
-func clean(svr *zookeeper.ZkService, znode string) error {
-	_, stat, err := svr.Conn.Get(znode)
-	if errors.Is(err, zk.ErrNoNode) {
-		return nil
-	} else if err != nil {
-		return errors.Wrap(err, znode)
-	}
-	base := path.Base(znode)
-	if base == cmdOps.Node {
-		if cmdOps.Dryrun {
-			fmt.Println(znode)
-		} else {
-			err = svr.DeleteAll(znode)
-			if err != nil {
-				fmt.Printf("znode %s delete failed: %v\n", znode, err)
-			} else {
-				fmt.Printf("znode %s delete success\n", znode)
-			}
-		}
-	} else if stat.NumChildren > 0 {
-		children, _, err := svr.Conn.Children(znode)
-		if err != nil {
-			return errors.Wrap(err, znode)
-		}
-
-		for _, child := range children {
-			subnode := path.Join(znode, child)
-			err := clean(svr, subnode)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
