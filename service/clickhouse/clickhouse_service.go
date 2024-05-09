f6f762617ab8d5fcaa783b735589a96a0d0366ea
YenchangChan <chenyanchang1990@163.com>
2024-05-09 16:40:17 +0800

    fix: since v24.3, disk type 'local' rename to 'Local'

--- service/clickhouse/clickhouse_service.go
+++ service/clickhouse/clickhouse_service.go
@@ -225,7 +225,7 @@ func getNodeInfo(service *CkService) (string, string) {
 	query := `SELECT
 	formatReadableSize(sum(total_space) - sum(free_space)) AS used,
 		formatReadableSize(sum(total_space)) AS total, uptime() as uptime
-	FROM system.disks WHERE type = 'local'`
+	FROM system.disks WHERE lower(type) = 'local'`
 	value, err := service.QueryInfo(query)
 	if err != nil {
 		return "NA/NA", ""
